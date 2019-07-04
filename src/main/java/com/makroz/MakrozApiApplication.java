a43a0c1b53a142244ed138d16f7bebd0ba3fc2ea
jshulk <vijaytyagi.vt@gmail.com>
2019-07-04 19:59:59 +0530

    Initial Commit

---
+++ src/main/java/com/makroz/MakrozApiApplication.java
@@ -0,0 +1,13 @@
+package com.makroz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakrozApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakrozApiApplication.class, args);
+	}
+
+}
